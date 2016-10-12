458a02c278d796af18f8c3da267a10046287d319
andyLiu <673988904@qq.com>
2016-10-12 13:55:05 +0800

    remove get permission SYSTEM_ALERT_WINDOW

--- app/src/main/java/org/ancode/alivehelperdemo/MainActivity.java
+++ app/src/main/java/org/ancode/alivehelperdemo/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initView();
         registerBroadCast();
 
-        initPermission();
+        //initPermission();
     }
 
     private void initPermission() {
